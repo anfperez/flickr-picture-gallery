e5b3931cb81028d82af49a93bc206c81749fbb2d
Angela Perez <anfperez@gmail.com>
2016-08-29 00:24:17 -0400

    finishing touches

--- js/script.js
+++ js/script.js
@@ -20,23 +20,23 @@ $(document).ready(function() {
         var secret = data.photos.photo[i].secret
 
       //the static address to photos on Flickr is accessed through this address: https://farm{farm-id}.staticflickr.com/{server-id}/{id}_{secret}.jpg
-      //this is the direct link to access photos on Flickr, minus the ".jpg" designation that will be added, according to whether we are trying to access the medium picture or the large picture
-        var pic_url = "https://farm" + farm_id + ".staticflickr.com/" + server_id + "/" + photo_id + "_" + secret
+      //this variable is the direct link to access photos on Flickr, minus the ".jpg" designation that will be added, according to whether we are trying to access the medium picture or the large picture
+        var pic_url = "https://farm" + farm_id + ".staticflickr.com/" + server_id + "/" + photo_id + "_" + secret;
 
         //this is the variable that stores the medium jpeg URL
-        var pic_url_m = pic_url + "_m.jpg"
+        var pic_url_m = pic_url + "_m.jpg";
 
         //this stores an image tag which will be populated with a medium jpeg URL
-        var pic_img = ('<img src=\'' + pic_url_m + '\' alt = \"pic\" />')
+        var pic_img = ('<img src=\'' + pic_url_m + '\' alt = \"pic\" />');
       
         //this appends the var pic_img to the photo_list div as the function loops through
 
         //$('.body').append('#frame');
-        $('#photo-list').append(pic_img)
+        $('#photo-list').append(pic_img);
 
         //this appends the class "paginate" to each img tag that is formed, ensuring that the the divs get passed to a later function called customPaginate
       //  $('img').addClass("paginate")
-        $('#photo-list img').addClass("paginate")
+        $('#photo-list img').addClass("paginate");
         
       }
 
@@ -48,17 +48,26 @@ $(document).ready(function() {
       //when img tags with the class paginate are clicked, the following function is called
       $('.paginate').click(function() {
 
-        //this variable saves the attribute of the the attr attribute
-        var src = $(this).attr('src')
+        //this variable saves the "src" or URL of (this) which is any element with the class "paginate"
+        var src = $(this).attr('src');
 
         //this variable takes the "src" variable, slices the last six characters, and replaces it with "_c.jpg", a large version of the image URL
-        var src_l = src.slice(0, -6) + "_c.jpg"  
+        var src_l = src.slice(0, -6) + "_c.jpg";
+
+        //ideally, I would also write functions that retrieve the actual height and width of the larger version of the image, and temporarily assign them to the #frame img tag so the user can see the true height and width of the images
+        //the below commented code is my attempt to grab this information, but I was not able to get it work successfully
+  //   $('#frame img').on('load', function(e) {
+  //   e.('#frame img').style.width = e.('#frame img').naturalWidth + 'px';
+  //   e.('#frame img').style.height = e.('#frame img').naturalHeight + 'px';
+  //   $(this).fadeIn();
+  //   $('#overlay').fadeIn();
+  // })
 
         //gives the "frame img" element a new attribute, which is the large image URL
-        $('#frame img').attr('src', src_l);
+       $('#frame img').attr('src', src_l);
 
         //allows the the "frame img" element to fade into the screen
-        $('#frame img').fadeIn();
+       $('#frame img').fadeIn();
 
         //allows the "overlay" element to fade onto the screen
         $('#overlay').fadeIn();
@@ -80,7 +89,6 @@ $(document).ready(function() {
   $.fn.customPaginate = function(options)
     {
       var paginationContainer = this;
-      var itemsToPaginate; 
            
       var defaults = {
         //sets how many items to a page
@@ -88,12 +96,15 @@ $(document).ready(function() {
            };
         
       var settings = {};
-
+      
+      //merges defaults and options into one one variable, settings 
       $.extend(settings, defaults, options);
-           
+      
+      //sets how many items will be on each page     
       var itemsPerPage = settings.itemsPerPage;
-        
-      itemsToPaginate = $(settings.itemsToPaginate);
+       
+      //sets which items are going to be 
+      var itemsToPaginate = $(settings.itemsToPaginate);
 
       //determines how many pages to generate based on the amount of items 
       var numberOfItems = Math.ceil((itemsToPaginate.length / itemsPerPage));
